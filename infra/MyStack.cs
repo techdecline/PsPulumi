ae9c1c6e0b424d7be52a680cee12c72e77be6e86
techdecline <cornelius.schuchardt@gmail.com>
2022-01-04 10:31:44 +0000

    function App

--- infra/MyStack.cs
+++ infra/MyStack.cs
@@ -30,13 +30,60 @@ class MyStack : Stack
         });
 
         var functionFileArchive = new FileArchive("../backend/handleSignup");
-        new AzureNative.Storage.Blob("function-code", new AzureNative.Storage.BlobArgs
+        var functionCodeBlob = new AzureNative.Storage.Blob("function-code", new AzureNative.Storage.BlobArgs
         {
             AccountName = storageAccount.Name,
             ResourceGroupName = resourceGroup.Name,
             ContainerName = blobContainer.Name,
             Source = functionFileArchive,
             BlobName = "source"
+        });
+
+        var servicePlan = new AzureNative.Web.AppServicePlan("windows-plan", new AzureNative.Web.AppServicePlanArgs
+        {
+            ResourceGroupName = resourceGroup.Name,
+            Sku = new AzureNative.Web.Inputs.SkuDescriptionArgs
+            {
+                Size = "Y1",
+                Tier = "Dynamic",
+                Name = "Y1"
+            }
+        });
+
+        var codeBlobUrl = SignedBlobReadUrl(functionCodeBlob, blobContainer, storageAccount, resourceGroup);
+
+        var webAppFunction = new AzureNative.Web.WebApp("functionApp", new AzureNative.Web.WebAppArgs
+        {
+            ResourceGroupName = resourceGroup.Name,
+            ServerFarmId = servicePlan.Id,
+            Kind = "FunctionApp",
+            SiteConfig = new AzureNative.Web.Inputs.SiteConfigArgs
+            {
+                AppSettings = new[]
+                {
+                    new AzureNative.Web.Inputs.NameValuePairArgs{
+                        Name = "runtime",
+                        Value = "node",
+                    },
+                    new AzureNative.Web.Inputs.NameValuePairArgs{
+                        Name = "WEBSITE_NODE_DEFAULT_VERSION",
+                        Value = "~12",
+                    },
+                    new AzureNative.Web.Inputs.NameValuePairArgs{
+                        Name = "FUNCTIONS_EXTENSION_VERSION",
+                        Value = "~3",
+                    },
+                    new AzureNative.Web.Inputs.NameValuePairArgs{
+                        Name = "FUNCTIONS_WORKER_RUNTIME",
+                        Value = "node",
+                    },
+                    new AzureNative.Web.Inputs.NameValuePairArgs{
+                        Name = "WEBSITE_RUN_FROM_PACKAGE",
+                        Value = codeBlobUrl,
+                    },
+                },
+            },
+            Name = "functionApp"
         });
 
         var storageAccountStaticWebsite = new AzureNative.Storage.StorageAccountStaticWebsite("staticWebsite", new AzureNative.Storage.StorageAccountStaticWebsiteArgs
@@ -69,5 +116,30 @@ class MyStack : Stack
                 ContentType = contentType
             });
         }
+
+        this.Endpoint = webAppFunction.DefaultHostName;
+    }
+
+    [Output] public Output<string> Endpoint { get; set; }
+
+    private static Output<string> SignedBlobReadUrl(AzureNative.Storage.Blob blob, AzureNative.Storage.BlobContainer container, AzureNative.Storage.StorageAccount account, AzureNative.Resources.ResourceGroup resourceGroup)
+    {
+        var serviceSasToken = AzureNative.Storage.ListStorageAccountServiceSAS.Invoke(new AzureNative.Storage.ListStorageAccountServiceSASInvokeArgs
+        {
+            AccountName = account.Name,
+            Protocols = AzureNative.Storage.HttpProtocol.Https,
+            SharedAccessStartTime = "2021-01-01",
+            SharedAccessExpiryTime = "2030-01-01",
+            Resource = AzureNative.Storage.SignedResource.C,
+            ResourceGroupName = resourceGroup.Name,
+            Permissions = AzureNative.Storage.Permissions.R,
+            CanonicalizedResource = Output.Format($"/blob/{account.Name}/{container.Name}"),
+            ContentType = "application/json",
+            CacheControl = "max-age=5",
+            ContentDisposition = "inline",
+            ContentEncoding = "deflate",
+        }).Apply(blobSAS => blobSAS.ServiceSasToken);
+
+        return Output.Format($"https://{account.Name}.blob.core.windows.net/{container.Name}/{blob.Name}?{serviceSasToken}");
     }
 }
